a02cfaf457d87c40560ab22c235a624aa08c6952
Kavipriya223 <kavipriyakarthi223@gmail.com>
2026-05-12 13:52:48 +0530

    middleware added

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,6 @@
 const User = require("../models/user");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const registerUser = async (req, res) => {
   try {
@@ -35,6 +36,61 @@ const registerUser = async (req, res) => {
   }
 };
 
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Check user exists
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({
+        message: "Invalid email or password",
+      });
+    }
+
+    // Compare password
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({
+        message: "Invalid email or password",
+      });
+    }
+
+    // Create token
+    const token = jwt.sign(
+      {
+        id: user._id,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "7d",
+      }
+    );
+
+    res.status(200).json({
+      message: "Login successful",
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+const getProfile = async (req, res) => {
+  res.status(200).json({
+    message: "Protected profile accessed",
+    user: req.user,
+  });
+};
 module.exports = {
-  registerUser,
+  registerUser,loginUser,getProfile,
 };
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,11 +1,16 @@
 const express = require("express");
+const protect=require("../middleware/authMiddleware")
 
 const router = express.Router();
 
 const {
   registerUser,
+  loginUser,
+  getProfile,
 } = require("../controllers/userController");
 
 router.post("/register", registerUser);
+router.post("/login",loginUser);
+router.get("/profile",protect,getProfile)
 
 module.exports = router;
